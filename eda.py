92d3c0f2db977eaa6351d70f5ad20ce5cfb17db4
advika sharmna <sharmaadvikaa@gmail.com>
2024-09-11 18:23:57 +0530

    EDA code and graphs

---
+++ eda.py
@@ -0,0 +1,32 @@
+import matplotlib.pyplot as plt
+
+#x=[1,2,3,4,5]
+#y=[2,3,5,7,11]
+##creating a line plot 
+#plt.plot(x,y,marker='o',color='Red')
+##add title and labels 
+#plt.title('Line Plot Example')
+#plt.xlabel('X-axis')
+#plt.ylabel('Y-axis')
+#plt.show()
+
+
+#x=[1,2,3,4,5]
+#y=[2,3,5,7,11]
+##creating a Scatter plot 
+#plt.scatter(x,y,color='green',s=100)
+##add title and labels 
+#plt.title('Scatter Plot Example')
+#plt.xlabel('X-axis')
+#plt.ylabel('Y-axis')
+#plt.show()
+
+
+#bar chart 
+categories=['A','B','C','D','E']
+value=[10,20,15,25,30]
+plt.bar(categories,value,color='Orange')
+plt.title('Bar Chart Example')
+plt.xlabel('Categorise')
+plt.ylabel('Value')
+plt.show()
